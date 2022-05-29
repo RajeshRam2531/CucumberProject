a6713a3108a4671384ccea3f62c4afb367800216
RajeshRam2531 <rajeshramamurthy2531@gmail.com>
2022-05-29 16:46:17 +0530

    Junit issue

--- src/test/java/Runner/RunnerClass.java
+++ src/test/java/Runner/RunnerClass.java
@@ -6,16 +6,14 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="FeatureFile/DataTable.feature" ,//to specify feature file path
+@CucumberOptions(features="FeatureFile/OpenOrangeHRM.feature" ,//to specify feature file path
 //glue={"stepDefinition","hooks"}, //Its combiles the feature file and step definition , additionally combines hooks with before and after
 
 glue={"stepDefinition"},
-dryRun = true,// to verify step definition and feature file
 monochrome=true, // to give report in console with readable format
-plugin ={"html:report/webReport"} // its give the report in asked format
-//tags="@Positive and @Smoke") // can specify which tag to run from multipls feature files
+plugin ={"html:report/webReport"} ,// its give the report in asked format
+tags="@Smoke") // can specify which tag to run from multipls feature files
 //tags="@Negative or @Smoke")
-)
 public class RunnerClass {
 	
 }
